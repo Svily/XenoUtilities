55ab97b328d20356390cd52b0aa3ddb1b63153a2
songqin <980047129@qq.com>
2023-02-21 11:07:34 +0800

    commit

---
+++ Runtime/Options.cs
@@ -0,0 +1,18 @@
+﻿namespace Xeno.Utilities
+{
+    /**
+     * 设置相关
+     */
+    
+    public enum SearchOptions
+    {
+        /// <summary>
+        /// 近似查找
+        /// </summary>
+        Approximate,
+        /// <summary>
+        /// 精准查找
+        /// </summary>
+        Accurate
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Xeno.Utilties.Test
+{
+    public class Test
+    {
+        public void Main()
+        {
+            Vector3 a = new Vector3(1, 1, 1);
+        }
+    }
+}
